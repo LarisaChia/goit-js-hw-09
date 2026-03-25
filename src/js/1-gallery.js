511ed26fd7fece3b6459d64f561d134ec6343667
LarisaChia <130046767+LarisaChia@users.noreply.github.com>
2026-03-25 20:35:33 +0000

    hw-9-4

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -64,27 +64,29 @@ const images = [
   },
 ];
 
-const gallery = document.querySelector('.gallery');
-
-const markup = images
-  .map(
-    ({ preview, original, description }) => `
-      <li class="gallery-item">
-        <a class="gallery-link" href="${original}">
-          <img
-            class="gallery-image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-          />
-        </a>
-      </li>`
-  )
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = images
+  .map(({ preview, original, description }) => {
+    return `
+    <li class="gallery-item">
+      <a class="gallery-link" href="${original}">
+        <img
+          class="gallery-image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>`;
+  })
   .join('');
+galleryContainer.innerHTML = galleryMarkup;
 
-gallery.innerHTML = markup;
+import SimpleLightbox from 'simplelightbox';
 
-new SimpleLightbox('.gallery a', {
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
   captionsData: 'alt',
   captionPosition: 'bottom',
   captionDelay: 250,
